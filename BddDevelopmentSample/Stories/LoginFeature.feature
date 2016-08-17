be1a9b9aec3878792ca6be5f35c61d9f6d71eff9
shreedhar <shreedhar@DESKTOP-SHREEDH>
2016-08-17 22:34:02 +0530

    Initial project creation and added login feature file with failing testt cases

---
+++ BddDevelopmentSample/Stories/LoginFeature.feature
@@ -0,0 +1,10 @@
+﻿Feature: LoginFeature
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Should login to application successfully
+	Given I have entered username in username field
+	And I have entered password in password field
+	When I click login
+	Then system should validate entered credentials and home page should be displayed
---
+++ BddDevelopmentSample/Tests/LoginFeatureTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TechTalk.SpecFlow;
+
+namespace BddDevelopmentSample.Tests
+{
+    [Binding]
+    public class LoginFeatureTests
+    {
+        private LoginModel model;
+        private ILoginProvider loginProvider;
+        public LoginFeatureTests()
+        {
+            model = new LoginModel();
+            loginProvider = new Mock<ILoginProvider>();
+        }
+
+        [Given(@"I have entered username in username field")]
+        public void GivenIHaveEnteredUsernameInUsernameField()
+        {
+            model.Username = "Test";
+        }
+
+        [Given(@"I have entered password in password field")]
+        public void GivenIHaveEnteredPasswordInPasswordField()
+        {
+            model.Password = "Test";
+        }
+
+        [When(@"I click login")]
+        public void WhenIClickLogin()
+        {
+            this.loginProvider.Login(model);
+        }
+
+        [Then(@"system should validate entered credentials and home page should be displayed")]
+        public void ThenSystemShouldValidateEnteredCredentialsAndHomePageShouldBeDisplayed()
+        {
+            Assert.AreEqual("Success", model.Message);
+        }
+
+
+
+    }
+}
